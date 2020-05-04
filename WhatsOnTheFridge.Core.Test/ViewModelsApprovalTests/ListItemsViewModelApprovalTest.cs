5cd627652a90d3aa4bd299d64ac348e752776af2
Juan <jjcv2304@hotmail.com>
2020-05-04 12:05:22 -0700

    Updating approval test

--- WhatsOnTheFridge.Core.Test/ViewModelsApprovalTests/ListItemsViewModelApprovalTest.cs
+++ WhatsOnTheFridge.Core.Test/ViewModelsApprovalTests/ListItemsViewModelApprovalTest.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using Akavache;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using Moq;
 using WhatsOnThe.Model;
-using WhatsOnTheFridge.Core.Test.Fakes;
 using WhatsOnTheFridge.Mobile.Core.Contracts.Services.Data;
 using WhatsOnTheFridge.Mobile.Core.Contracts.Services.General;
-using WhatsOnTheFridge.Mobile.Core.Services.Data;
 using WhatsOnTheFridge.Mobile.Core.ViewModels;
 using Xunit;
 using WhatsOnTheFridge.Core.Test.ViewModelsApprovalTests.Extensions;
-using System.Reactive.Linq;
 
 namespace WhatsOnTheFridge.Core.Test.ViewModelsApprovalTests
 {
@@ -24,7 +18,7 @@ namespace WhatsOnTheFridge.Core.Test.ViewModelsApprovalTests
   {
 
     [Fact]
-    [UseReporter(typeof(DiffReporter))]
+    [UseReporter(typeof(TortoiseDiffReporter))]
     public async Task AllItems_GetLoaded_AfterInitializeAsync()
     {
       var mockNavigationService = new Mock<INavigationService>();
@@ -33,7 +27,7 @@ namespace WhatsOnTheFridge.Core.Test.ViewModelsApprovalTests
       var fakeItemsList = Task.Run(() =>
         new List<Item>()
         {
-          new Item() {Id = 1, Name = "Test 1", Description = "Description 12", Quantity = 1},
+          new Item() {Id = 1, Name = "Test 1", Description = "Description 123", Quantity = 1},
           new Item() {Id = 3, Name = "Test 3", Description = "Description 3", Quantity = 3}
         }
           .AsEnumerable()
